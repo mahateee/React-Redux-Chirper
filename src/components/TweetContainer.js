69b31679eb98256eeef56a1da64b4094318028fe
Maha AlShehri <mhoneymohamed@hotmail.com>
2023-09-09 18:55:13 +0300

    update tweet container height

--- src/components/TweetContainer.js
+++ src/components/TweetContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TweetContainer({ children }) {
   return (
-    <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
+    <section class="bg-white min-h-screen dark:bg-gray-900 py-8 lg:py-16 antialiased">
       <div class="max-w-2xl mx-auto px-4">{children}</div>
     </section>
   );
